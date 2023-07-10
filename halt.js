d3bf4969d4501b19fca22efcf96e197b41abbce0
Kelvin Tay <kelvintaywl@gmail.com>
2023-07-10 12:10:25 +0900

    Update halt.js

--- halt.js
+++ halt.js
@@ -3,4 +3,5 @@ const proc = require('child_process')
 if (process.env.CIRCLE_NODE_INDEX == "1") {
     console.log("Halting...")
     proc.execSync(`circleci-agent step halt`, { cwd: process.cwd() })
+    throw new Error("Test if this raises")
 }
